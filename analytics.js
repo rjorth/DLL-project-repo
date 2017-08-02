5361bdb189a22b633fe70ba0bf2332bf2017d368
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-08-02 15:14:23 -0400

    that should do it

---
+++ analytics.js
@@ -0,0 +1,24 @@
+﻿function multiplyBy() {
+ 
+ num1 = document.getElementById("firstNumber").value;
+ num2 = document.getElementById("secondNumber").value;
+ document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() {
+
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+}
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello World",10,50);
+
+function funcNavigate() {
+
+ window.navigate("apply.html"); 
+
+}
